0455a750f173b707099f253c1c770789c977dfd0
Iker Gallardo Arrieta <ikergallardo22@gmail.com>
2026-01-16 17:35:15 +0100

    Randomly generated fake blocks.
    Player initial block avoided to be a fake block. Exit door object created, but not implemented.

--- Assets/Scripts/FloorExercise/FloorSceneController.cs
+++ Assets/Scripts/FloorExercise/FloorSceneController.cs
@@ -4,25 +4,29 @@ using System.Collections.Generic;
 public class FloorSceneController : MonoBehaviour
 {
     // Plublic Attributes
-    public GameObject[] floorBlocks = new GameObject[2]; // Array to hold different types of floor blocks
+    public GameObject[] floorBlocks = new GameObject[3]; // Array to hold different types of floor blocks
     [Range(20, 100)]
     public int numberOfFakeBlocks = 50; // Total number of blocks to be placed
 
     // Private Attributes
     private List<int> fakeBlockIndexes = new List<int>(); // Array to hold indices for floor block types
     private int blockCounter = 0; // Counter for the number of blocks placed
+    private int selectedFakeBlockIndex = 1; // Index for selecting fake block type
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Generate unique random indexes for fake blocks
         for (int i = 0; i < numberOfFakeBlocks; i++)
         {
             int randomIndex = Random.Range(1, 625);
-            print("Random Index: " + randomIndex);
+            if (fakeBlockIndexes.Contains(randomIndex) || randomIndex == 313) // Avoid duplicates and player's initial block
+            {
+                i--;
+                continue;
+            }
             fakeBlockIndexes.Add(randomIndex);
         }
-
-        print("Fake Block Indexes: " + fakeBlockIndexes);
 
         // Initialize floor blocks randomly
         for (int i = 0; i < 25; i++)
@@ -33,11 +37,15 @@ public class FloorSceneController : MonoBehaviour
                 Vector3 position = new Vector3((i * 2.0f + 1f), -0.5f, (j * 2.0f + 1f));
                 if (fakeBlockIndexes.Contains(blockCounter))
                 {
-                    Instantiate(floorBlocks[1], position, Quaternion.identity);
+                    selectedFakeBlockIndex = Random.Range(1, 3);
+                    print("Selected Fake Block Index: " + selectedFakeBlockIndex);
+                    GameObject CreatedBlock = Instantiate(floorBlocks[selectedFakeBlockIndex], position, Quaternion.identity);
+                    CreatedBlock.name = "FakeBlock_" + blockCounter;
                 }
                 else
                 {
-                    Instantiate(floorBlocks[0], position, Quaternion.identity);
+                    GameObject CreatedBlock = Instantiate(floorBlocks[0], position, Quaternion.identity);
+                    CreatedBlock.name = "RealBlock_" + blockCounter;
                 }
             }
         }
