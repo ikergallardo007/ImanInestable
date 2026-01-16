301653c520a2c829eb7571204d6c4fd939d9b029
Iker Gallardo Arrieta <ikergallardo22@gmail.com>
2026-01-16 18:13:16 +0100

    Random position of the exit door implemented.
    Next Step: Fake blocks mechanichs implementation.

--- Assets/Scripts/FloorExercise/FloorSceneGenerator.cs
+++ Assets/Scripts/FloorExercise/FloorSceneGenerator.cs
@@ -1,21 +1,48 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
-public class FloorSceneController : MonoBehaviour
+public class FloorSceneGenerator : MonoBehaviour
 {
     // Plublic Attributes
-    public GameObject[] floorBlocks = new GameObject[3]; // Array to hold different types of floor blocks
+    public GameObject exitDoor; // Reference to the exit door GameObject
     [Range(20, 100)]
     public int numberOfFakeBlocks = 50; // Total number of blocks to be placed
+    public GameObject[] floorBlocks = new GameObject[3]; // Array to hold different types of floor blocks
 
     // Private Attributes
     private List<int> fakeBlockIndexes = new List<int>(); // Array to hold indices for floor block types
     private int blockCounter = 0; // Counter for the number of blocks placed
     private int selectedFakeBlockIndex = 1; // Index for selecting fake block type
+    private int exitDoorWallIndex;
+    private int exitDoorPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Select a random wall index for the exit door (0: North, 1: East, 2: South, 3: West) and a random position along that wall
+        exitDoorWallIndex = Random.Range(0, 4);
+        exitDoorPosition = Random.Range(0, 25);
+        switch (exitDoorWallIndex)
+        {
+            case 0: // North Wall4
+                print("North Wall Selected");
+                exitDoor.transform.position = new Vector3((2 * exitDoorPosition) + 1,0.5f,50.4f);
+                break;
+            case 1: // East Wall3
+                print("East Wall Selected");
+                exitDoor.transform.position = new Vector3(50.4f, 0.5f, (2 * exitDoorPosition) + 1);
+                break;
+            case 2: // South Wall1
+                print("South Wall Selected");
+                exitDoor.transform.position = new Vector3((2 * exitDoorPosition) + 1, 0.5f, -0.4f);
+                break;
+            case 3: // West Wall2
+                print("West Wall Selected");
+                exitDoor.transform.position = new Vector3(-0.4f, 0.5f, (2 * exitDoorPosition) + 1);
+                break;
+        }
+
         // Generate unique random indexes for fake blocks
         for (int i = 0; i < numberOfFakeBlocks; i++)
         {
@@ -38,7 +65,6 @@ public class FloorSceneController : MonoBehaviour
                 if (fakeBlockIndexes.Contains(blockCounter))
                 {
                     selectedFakeBlockIndex = Random.Range(1, 3);
-                    print("Selected Fake Block Index: " + selectedFakeBlockIndex);
                     GameObject CreatedBlock = Instantiate(floorBlocks[selectedFakeBlockIndex], position, Quaternion.identity);
                     CreatedBlock.name = "FakeBlock_" + blockCounter;
                 }
